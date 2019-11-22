c22efcfa5e74e0f2fd4069f75224fb79094312c2
tlanders <tlanders@users.noreply.github.com>
2019-11-21 20:24:04 -0600

    added function to look for a winner preventing invalid moves

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();     // make copy of squares array
+        if(squares[i] || calculateWinner(this.state.squares)) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({squares: squares, xIsNext: !this.state.xIsNext});
     }
@@ -34,7 +37,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -73,6 +82,27 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for(let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
